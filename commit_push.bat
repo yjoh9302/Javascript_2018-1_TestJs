071fb53ea2689f9f545e64bd6a77623db21c9832
yjoh9302 <ohyj48@naver.com>
2018-03-27 12:25:21 +0900

    Javascript_2018-1_TestJs_mypage

--- commit_push.bat
+++ commit_push.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "Javascript_2018-1_TestJs_myImg"
+git commit -m "Javascript_2018-1_TestJs_mypage"
 git push -u origin master
--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
 var myHeading = document.querySelector('h1');
-myHeading.innerHTML = 'Cloud Service<br>2018-1'; 
+var id =localStorage.getItem('id');
+myHeading.innerHTML = id + 'Homepage'; 
 
 function getIdPw(){
 var id = prompt('ID 입력', '');
 alert(id + '가 로그인 함');
+localStorage.setItem('id', id);
 var passwordSystem = '12345';
 var password =prompt('PW 입력','');
 if(password === passwordSystem) alert('정상 로그인');
@@ -11,9 +13,9 @@ else alert('비밀번호 오류');
 }
 //getIdPw();
 //자바스트립트는 함수를 객체로 처리함
-document.querySelector('h1').onclick = function() {
+/*document.querySelector('h1').onclick = function() {
 getIdPw();//만약 html 대신 h1이면 글씨를 클릭해야 로그인 창이 뜸
-}
+}*/
 
 var myImg = document.querySelector('img');
 myImg.onclick = function () {
@@ -21,4 +23,9 @@ myImg.onclick = function () {
     if(src === 'images/firefox-icon.png')
     myImg.setAttribute('src', 'images/mokwon.png');
     else myImg.setAttribute('src', 'images/firefox-icon.png');
+}
+
+var myButton = document.querySelector('button');
+myButton.onclick = function () {
+getIdPw();
 }
